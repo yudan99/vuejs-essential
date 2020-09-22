760818f82ac5a2e7f334f124ffc0302d46a03254
YaoXin <2222122099@qq.com>
2020-09-22 16:35:31 +0800

    add github pages

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
-<template>
+<!-- <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Welcome to Your Vue.js App888"/>
   </div>
 </template>
 
@@ -24,5 +24,56 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+</style> -->
+
+<!-- <template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <h1>Welcome to Learnku Vue.js.com</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 200px;
+}
+</style> -->
+
+<template lang="pug">
+  div#app
+    img(src="./assets/logo.png")
+    h1 Welcome from pug & sass
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 200px;
+
+  h1 {
+    color: #42b983;
+  }
 }
 </style>
